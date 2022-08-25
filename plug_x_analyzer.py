f5a188249f72c3ba3cebd6e939c55eddc3992d3c
Felipe Duarte <8562692+dark0pcodes@users.noreply.github.com>
2022-08-25 16:45:24 -0500

    Update plug_x_analyzer.py
    Support for x86 binaries was added

--- plug_x_analyzer.py
+++ plug_x_analyzer.py
@@ -8,28 +8,88 @@ import pefile
 
 def read_file(file_path):
     """
-    Return given a file path return a buffer with the data
+    Given a file path return a buffer with the data
 
     :param file_path:
     :return:
     """
     with open(file_path, 'rb') as f:
-        data = f.read()
-    return data
+        buffer = f.read()
+    return buffer
 
 
-def xor_decrypt(data):
+def save_file(file_path, buffer):
+    """
+    Given a file path and a buffer creates a file in disk
+
+    :param file_path:
+    :param buffer:
+    :return:
+    """
+    with open(file_path, 'wb') as f:
+        f.write(buffer)
+
+
+def brute_force_location(buffer, target, window=512):
+    """
+    Given an encrypted buffer, the window and the target word return the starting address of the encrypted data
+
+    :param buffer:
+    :param target:
+    :param window:
+    :return:
+    """
+    index = 0
+
+    while True:
+        tmp = xor_decrypt(buffer[index:index + window])
+
+        if target in tmp:
+            break
+
+        index += 1
+    return index
+
+
+def brute_force_pe_location(buffer):
+    """
+    Given an encrypted buffer containing a PE, returns the starting address of the encrypted PE
+
+    :param buffer:
+    :return:
+    """
+    return brute_force_location(buffer, b'This')
+
+
+def brute_force_pe_extraction(buffer, address):
+    """
+    Given a buffer and the PE start address returns the decrypted PE
+
+    :param buffer:
+	:param address:
+    :return:
+    """
+    stop = 2500
+
+    while True:
+        try:
+            return lznt1.decompress(xor_decrypt(buffer[address:address + stop])[16:])
+        except ValueError:
+            stop += 1
+
+
+def xor_decrypt(buffer):
     """
     Decrypt PlugX payload and config
 
-    :param data:
+    :param buffer:
     :return:
     """
-    key = struct.unpack('<I', data[0:4])[0]
+    key = struct.unpack('<I', buffer[0:4])[0]
     key_a, key_b, key_c = key, key, key
     result = bytes([])
 
-    for char in data:
+    for char in buffer:
         key = (key + (key >> 3) - 0x11111111) & 0xFFFFFFFF
         key_a = (key_a + (key_a >> 5) - 0x22222222) & 0xFFFFFFFF
         key_b = (key_b - (key_b << 7) + 0x33333333) & 0xFFFFFFFF
@@ -38,27 +98,25 @@ def xor_decrypt(data):
         result += bytes([char ^ ((key + key_a + key_b + key_c) & 0x000000FF)])
 
     return result
-    
+
 
 def get_data_section(file_path):
     """
     Locate configuration section
-    
+
     :param file_path:
     :return:
     """
     return pefile.PE(file_path).sections[2].get_data()
 
 
-def main(file_path):
+def x64_analysis(sc):
     """
-    Main logic of the script
+    Run analysis logic for x64 binaries
 
+    :param sc:
     :return:
     """
-    print(f'[+] Analyzing shellcode at "{file_path}"')
-    sc = read_file(file_path)
-
     # Find main shellcode function
     print('[+] Finding main shellcode function')
     main_index = sc.index(b'\xe8\x05', 0, 96)
@@ -69,56 +127,108 @@ def main(file_path):
     dll_ptr = struct.unpack('<I', sc[dll_index:dll_index + 4])[0]
 
     # Find DLL size
+    print('[+] Extracting size of the encrypted DLL')
     size_index = main_index - 24
     size = struct.unpack('<I', sc[size_index:size_index + 4])[0]
-    print('[+] Extracting size of the encrypted DLL')
 
     # Extract DLL
     print('[+] Decrypting PlugX DLL')
     dll_data = lznt1.decompress(xor_decrypt(sc[dll_ptr:dll_ptr + size])[16:])
-    
 
     # Saving DLL
     print('[+] Saving PlugX DLL')
-    with open('plug_x_dll.bin', 'wb') as f:
-        f.write(dll_data)
+    save_file('plug_x_dll.bin', dll_data)
 
     # Extracting embedded DLL
     print('[+] Extracting embedded DLL (Privilege Escalation)')
-    data = get_data_section('plug_x_dll.bin')
-    index = 0
-    
-    while True:
-        tmp = xor_decrypt(data[index:index + 512])
-        
-        if b'This' in tmp:
-            break
-
-        index += 1
-
-    embedded_dll = lznt1.decompress(xor_decrypt(data[index:index + 2921])[16:])
-    with open('plug_x_embedded_dll.bin', 'wb') as f:
-        f.write(embedded_dll)
+    section_data = get_data_section('plug_x_dll.bin')
+    embedded_dll_data = brute_force_pe_extraction(section_data, brute_force_pe_location(section_data))
+    save_file('plug_x_embedded_dll.bin', embedded_dll_data)
 
     # Extract config
     print('[+] Decrypting attack configuration')
-    config = xor_decrypt(sc[dll_ptr + size:])
-    servers = re.finditer(b'(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]', config)
+    config_data = xor_decrypt(sc[dll_ptr + size:])
+    save_file('config.bin', config_data)
 
-    # Saving config
-    print('[+] Saving attack configuration')
-    with open('config.bin', 'wb') as f:
-        f.write(config)
+    servers = re.finditer(b'(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]', config_data)
 
     print('[+] C2 servers:')
     for server in servers:
-        port = struct.unpack("<I", config[server.start() - 2:server.start()] + b"\x00\x00")[0]
-        print(f'\t[-] {config[server.start():server.end()].decode()}:{port}')
+        port = struct.unpack("<I", config_data[server.start() - 2:server.start()] + b"\x00\x00")[0]
+        print(f'\t[-] {config_data[server.start():server.end()].decode()}:{port}')
+
+
+def x32_analysis(sc):
+    """
+    Run analysis logic for x32 binaries
+
+    :param sc:
+    :return:
+    """
+    # Find DLL pointer in shellcode
+    print('[+] Extracting address of the encrypted DLL')
+    dll_ptr = brute_force_pe_location(sc)
+
+    # Find DLL size
+    print('[+] Extracting size of the encrypted DLL')
+    size_index = dll_ptr - 9
+    size = struct.unpack('<I', sc[size_index:size_index + 4])[0]
+
+    # Extract DLL
+    print('[+] Decrypting PlugX DLL')
+    dll_data = lznt1.decompress(xor_decrypt(sc[dll_ptr:dll_ptr + size])[16:])
+
+    # Saving DLL
+    print('[+] Saving PlugX DLL')
+    save_file('plug_x_dll.bin', dll_data)
+
+    # Extracting embedded DLL
+    print('[+] Extracting embedded DLL (Privilege Escalation)')
+    section_data = get_data_section('plug_x_dll.bin')
+    embedded_dll_data = brute_force_pe_extraction(section_data, brute_force_pe_location(section_data))
+    save_file('plug_x_embedded_dll.bin', embedded_dll_data)
+
+    # Extract config
+    print('[+] Decrypting attack configuration')
+    config_size = 5388
+    confid_ptr = brute_force_location(sc, b'HTTP', config_size)
+    config_data = xor_decrypt(sc[confid_ptr:confid_ptr + config_size])
+    save_file('config.bin', config_data)
+
+    servers = re.finditer(b'(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]', config_data)
+
+    print('[+] C2 servers:')
+    for server in servers:
+        port = struct.unpack("<I", config_data[server.start() - 2:server.start()] + b"\x00\x00")[0]
+        print(f'\t[-] {config_data[server.start():server.end()].decode()}:{port}')
+
+
+def main(file_path):
+    """
+    Main logic of the script
+
+    :return:
+    """
+    print(f'[+] Analyzing shellcode at "{file_path}"')
+    sc = read_file(file_path)
+    try:
+        x64_analysis(sc)
+    except:
+        x32_analysis(sc)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PlugX Payload and Config Extractor.')
     parser.add_argument('file_path', type=str, help='File path of the PlugX Shellcode')
+    parser.add_argument('--mode', type=str, default='sc', help='Shellcode analysis')
 
     args = parser.parse_args()
-    main(args.file_path)
+
+    if args.mode != 'config':
+        main(args.file_path)
+    else:
+        print(f'[+] Analyzing config file at "{args.file_path}"')
+        data = read_file(args.file_path)
+
+        with open('config.bin', 'wb') as f:
+            f.write(xor_decrypt(data))
